6c0881b76de1e65d025d466599ead33e9253c6b2
cjsotoparra <cjsotoparra@email.arizona.edu>
2020-11-20 01:36:05 -0700

    Added routes and views to index for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,18 @@ def show
 	@article = Article.find(params[:id])
 end
 
+def index
+	@articles = Article.all
+end
+
 =begin
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
   # GET /articles.json
-  def index
-    @articles = Article.all
-  end
+ # def index
+  #  @articles = Article.all
+  #end
 
   # GET /articles/1
   # GET /articles/1.json
