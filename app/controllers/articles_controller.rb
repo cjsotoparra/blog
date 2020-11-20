0057fd09b1ea9b7f7367589877c28c5c4cb9eaf8
cjsotoparra <cjsotoparra@email.arizona.edu>
2020-11-20 06:12:45 -0700

    Edit and update: update existing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ def new
 	@article = Article.new
 end
 
+def edit
+	@article = Article.find(params[:id])
+end
+
 def create
 	@article = Article.new(params.require(:article).permit(:title, :description))
 	if @article.save
@@ -20,6 +24,18 @@ def create
 		render 'new'
 	end
 end
+
+def update
+	@article = Article.find(params[:id])
+	
+	if @article.update(params.require(:article).permit( :title, :description))
+		flash[ :notice ] = "Article was updated successfully!"
+		redirect_to @article
+	else
+		render 'edit'
+	end
+end
+
 =begin
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
